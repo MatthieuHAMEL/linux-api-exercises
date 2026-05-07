1d318cb2806dc33c5e8b64ca902162d0b613b59a
MatthieuHAMEL <hamelmatthieusolo@gmail.com>
2026-05-07 21:13:01 +0200

    readv, writev (centralized w/ templates)

--- src/5_MoreFileIO/reimpl.cpp
+++ src/5_MoreFileIO/reimpl.cpp
@@ -37,7 +37,39 @@ namespace Reimpl
 
     return ::fcntl(oldfd, F_DUPFD, newfd);
   }
+
+  template <typename BufType, ssize_t ReadOrWrite(int fd, BufType buf, size_t cnt)>
+  ssize_t ReadOrWritev(int fd, const struct iovec *iov, int iovcnt)
+  {
+    if (!iov) [[unlikely]]
+    {
+      errno = EINVAL;
+      return -1;
+    }
+
+    ssize_t total = 0;
+    for (int i = 0; i < iovcnt; ++i)
+    {
+      ssize_t rc = ReadOrWrite(fd, iov[i].iov_base, iov[i].iov_len);
+      if (rc == -1) [[unlikely]] // propagate
+        return -1;
+      total += rc;
+      if (static_cast<size_t>(rc) != iov[i].iov_len) // not everything has been read/written so no need to write iov[i+1], etc.
+        break;
+    }
+    return total;
+  }
+
+  ssize_t writev(int fd, const struct iovec* iov, int iovcnt)
+  {
+    return ReadOrWritev<const void*, &::write>(fd, iov, iovcnt);
+  }
+  ssize_t readv(int fd, const struct iovec* iov, int iovcnt)
+  {
+    return ReadOrWritev<void*, &::read>(fd, iov, iovcnt);
+  }
 }
+
 auto main() -> int
 {
   int fd1 = open("babar.txt", O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
@@ -48,5 +80,21 @@ auto main() -> int
   lseek(fd2, 0, SEEK_SET);
   write(fd1, "HELLO,", 6);
   write(fd3, "Gidday", 6);
+
+  constexpr std::string_view STR0 = "first ";
+  constexpr std::string_view STR1 = "second \n";
+  struct iovec iov[2];
+  iov[0].iov_base = const_cast<void*>(reinterpret_cast<const void*>(STR0.data()));
+  iov[0].iov_len = STR0.length();
+  iov[1].iov_base = const_cast<void*>(reinterpret_cast<const void*>(STR1.data()));
+  iov[1].iov_len = STR1.length();
+
+  ssize_t nwritten = Reimpl::writev(fd1, iov, 2);
+  if (nwritten == -1)
+    return Ham::Diag("Error in writev", EXIT_FAILURE);
+  
+  printf("written -> %zu\n", static_cast<size_t>(nwritten));
+
+  
   return EXIT_SUCCESS;
 }
