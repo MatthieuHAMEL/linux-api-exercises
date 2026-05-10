cfe6d8be2037a363f7e91e313e164270cef32fcb
MatthieuHAMEL <hamelmatthieusolo@gmail.com>
2026-05-10 19:12:06 +0200

    malloc reimplementation ok; free in progress

--- src/7_MemoryAllocation/allocator.cpp
+++ src/7_MemoryAllocation/allocator.cpp
@@ -1,42 +1,75 @@
 // my own malloc
+// - not atomic, not threadsafe
+// - does not return 16-bytes aligned memory
+// - homemade and certainly not as efficient as glibc
 
 #include <cassert>
 #include <unistd.h>
 #include <cstdint>
+#include <cstdio>
 
 import std;
 
-namespace Reimpl
+namespace Ham
 {
   struct FreeBlock
   {
-    size_t sz = 0;                // sizeof(size_t) bytes before the pointer returned by malloc()
+    size_t sz = 0;                // sizeof(size_t) bytes before the pointer returned by malloc(), includes the size of 'next'
     FreeBlock* next = nullptr;    // coincides with the pointer returned by malloc()
   };
   
   FreeBlock* FREELIST = nullptr; // Sorted (by construction) single linked list
   const int PAGESIZE = getpagesize();
-  
-  void* malloc(size_t size)
+  const char* HAMTRACE = std::getenv("HAMTRACE"); // simple stdout tracing
+
+  int i = 0;
+  void DumpFreeList()
   {
+    puts("FREELIST AT STEP");
+    printf("%d\n",i++);
+    // print the freelist
+    int j = 0;
+    for (FreeBlock** ppFreeBlock = &FREELIST; *ppFreeBlock; ppFreeBlock = &(*ppFreeBlock)->next)
+    {
+      printf("B%d[sz=", j++);
+      printf("%zu",(*ppFreeBlock)->sz);
+      printf("; next=");
+      printf("%p]\n",(*ppFreeBlock)->next); fflush(stdout);
+    }
+    
+  }
+  
+  void* malloc(size_t requested)
+  {
+    // change the input size to a multiple of 8 bytes so that when it becomes a freeblock there is room for the 'next' pointer
+    size_t size = (requested / sizeof(size_t)) * sizeof(size_t);
+    if (requested != size) [[likely]] size += sizeof(size_t);
+    
+    if (HAMTRACE)
+    {
+      DumpFreeList();
+    }
     // Try to find a suitable block in the freelist
     FreeBlock** ppFreeBlock = &FREELIST;
     for (; *ppFreeBlock; ppFreeBlock = &(*ppFreeBlock)->next)
     {
       if ((*ppFreeBlock)->sz >= size) // The requested size fits in the block
       {
-        void* result = static_cast<void*>(*ppFreeBlock + sizeof(size_t)); // Return the zone after the size
+        // result == the zone after the size (size_t). In the code I avoid pointer arithmetics by casting the pointers to size_t
+        void* result = reinterpret_cast<void*>(reinterpret_cast<size_t>(*ppFreeBlock) + sizeof(size_t));
         if ((*ppFreeBlock)->sz == size)
         {
           // cool: it fits exactly. Just remove the block and return
           *ppFreeBlock = (*ppFreeBlock)->next; // (cur == prev->next now points to pNewFreeblock)
           return result;
         }
-        else if ((*ppFreeBlock)->sz >= size + sizeof(size_t))
+        else if ((*ppFreeBlock)->sz >= size + 2*sizeof(size_t))
         {
           // Split the block, the first sz part is returned, the other is a new freeblock
-          // I ask for sz > size + sizeof(size_t) since the new freeblock has to have minimum 2 bytes (sz, next)
-          FreeBlock* pNewFreeBlock = reinterpret_cast<FreeBlock*>(result + size);
+          // I ask for sz > size + 2*sizeof(size_t) since the new freeblock has to have minimum 2 bytes (sz, next)
+          FreeBlock* pNewFreeBlock = reinterpret_cast<FreeBlock*>(reinterpret_cast<size_t>(result) + size); // size_t cast to avoid ptr arithmetic
+
+          // size is rounded to 8N bytes, so pNewfreeblock->sz does not overlap with (*ppFreeblock)->next even when requested < 8
           pNewFreeBlock->sz = (*ppFreeBlock)->sz - sizeof(size_t) - size;
           pNewFreeBlock->next = (*ppFreeBlock)->next;
           
@@ -48,37 +81,64 @@ namespace Reimpl
       }
     }
 
-    // At this point, I didn't find any matching block on the freelist. Increase the program break, represent its beginning as a FreeBlock
-    // unless size is a multiple of PAGESIZE (which I ignore) I still need 16 bytes to represent a FreeBlock following the returned memory
-    const size_t allocated = ((size + 2*sizeof(size_t) / PAGESIZE) + 1) * PAGESIZE;
+    /* At this point, I didn't find any matching block on the freelist. I increase the program break.
+       Unless size is a multiple of PAGESIZE (I ignore that case for now (cf. (1)) I will need a trailing freeblock.
+       So in addition to 'size' I still need a size_t for the size of the returned block
+       +2 size_t at least to represent a FreeBlock following the returned memory
+
+       (1) The worst thing that could happen is a loop of malloc(size) with size+8 multiple of PAGESIZE
+       ... instead of allocating 1 page at a time, I'll allocate 2 pages, the 2nd one would contain an empty freeblock of 4088 bytes
+       Though the libc malloc() may need more anyway or may not be aligned on a 4096B boundary at all. */
+    const size_t allocated = (((size + 3*sizeof(size_t)) / PAGESIZE) + 1) * PAGESIZE;
     FreeBlock* formerPBRK = reinterpret_cast<FreeBlock*>(sbrk(allocated));
     if (reinterpret_cast<void*>(-1) == formerPBRK) // allocation failed
       return nullptr; // propagate errno
     formerPBRK->sz = size; // next doesn't matter; this freeblock will not be in the list, it'll be returned.
+    
     // NB: the last freeblock may (or may not) be adjacent to the newly alloc'd space. It's ignored here for time-efficiency purposes.
     // Memory-efficiency idea:
     // IFF the last free block is adjacent to the program break, increase the program break by only (size - LastFreeBlockSize) upper PAGESIZE multiple
     // This could save a lot of memory, e.g the requested size is 1GB, the last freeblock is 999 MB long;
-    // we'd only need like 2 pages instead; in the current implementation I allocate 1GB. Of course this is an extreme case and the 999 MB will not be lost in future allocations!
-    FreeBlock* newLastFreeBlock = reinterpret_cast<FreeBlock*>(formerPBRK + sizeof(size_t) + size);
+    // we'd only need like 2 pages instead; in the current implementation I allocate 1GB (a few hundreds pages). Of course this is an extreme case and the 999 MB in the middle are not lost for future allocations!
+    FreeBlock* newLastFreeBlock = reinterpret_cast<FreeBlock*>(reinterpret_cast<size_t>(formerPBRK) + sizeof(size_t) + size); // size_t cast == avoid pointer arithmetic
     newLastFreeBlock->sz = allocated - size - 2*sizeof(size_t); // substract the size of the 1st block (size+data) and the newLast size
     newLastFreeBlock->next = nullptr;
 
     // Have the former last free block point on the new last free block
     *ppFreeBlock = newLastFreeBlock;
     
-    return formerPBRK + sizeof(size_t);
+    return reinterpret_cast<void*>(reinterpret_cast<size_t>(formerPBRK) + sizeof(size_t));
   }
 
   void free(void* ptr)
   {
     // TODO
+    ++i;
+
+    if (!ptr) [[unlikely]]
+      return;
+    
+    // If a freeblock is pointing on the newly released zone, it could be merged with the current one
+    FreeBlock* pNewFreeBlock = reinterpret_cast<FreeBlock*>(reinterpret_cast<size_t>(ptr) - sizeof(size_t));
+
+    // Loop on existing freeblocks; the freelist is sorted. No need to go past pNewfreeBlock
+    FreeBlock** ppFreeBlock = &FREELIST;
+    for (; *ppFreeBlock; ppFreeBlock = &(*ppFreeBlock)->next)
+    {
+      
+    }
+
+    
   }
 }
 
-// Basic test program ...
-// !! I obviously cannot use any C++ code allocating with the real malloc() or new !
-// ... it would badly conflict with Reimpl::malloc() and Reimpl::free() which uses its own freelist.
+/* Basic test program ...
+   !! I obviously cannot use any C++ code allocating with the real malloc() or new !
+   ... it would badly conflict with Ham::malloc() and Ham::free() which uses its own freelist.
+   ... At least while my allocator runs. Any new/malloc/... happening BEFORE any Ham::malloc/free work
+   do not conflict since my freelist starts empty : at its first call Ham::malloc will go past the program break, hence
+   ignoring any existing allocated zone.
+*/
 
 struct Chunk // "metainformation"
 {
@@ -109,13 +169,18 @@ auto main() -> int
     // If it is allocated, free it
     if (chunk.ptr)
     {
-      Reimpl::free(chunk.ptr);
+      Ham::free(chunk.ptr);
       chunk.ptr = nullptr;
     }
     else // allocate it
     {
       chunk.sz = randomNumber(10000);
-      chunk.ptr = Reimpl::malloc(chunk.sz);
+      if (Ham::HAMTRACE)
+      {
+        printf("step %d needs %zu\n", i, chunk.sz);
+      }
+
+      chunk.ptr = Ham::malloc(chunk.sz);
       assert(chunk.ptr);
     }
   }
@@ -126,12 +191,12 @@ auto main() -> int
     auto& someChunk = CHUNKS[12 + i];
     if (someChunk.ptr)
     {
-      Reimpl::free(someChunk.ptr);
+      Ham::free(someChunk.ptr);
       someChunk.ptr = nullptr;
     }
     // 0 bytes to 64 bytes allocation
     someChunk.sz = i;
-    someChunk.ptr = Reimpl::malloc(i);
+    someChunk.ptr = Ham::malloc(i);
   }
   
   // Free everything
@@ -139,7 +204,7 @@ auto main() -> int
   {
     if (chunk.ptr)
     {
-      Reimpl::free(chunk.ptr);
+      Ham::free(chunk.ptr);
       chunk.ptr = nullptr;
     }
   }
